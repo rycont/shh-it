5680ec726670dc1ad7dc8085f884f742264eead8
RyCont(정한) <35295182+rycont@users.noreply.github.com>
2021-01-18 12:12:45 +0900

    refactor : cleaning by using fetch / promise
    Fetch와 Promise를 사용해서 더 깔끔한 코드로 리팩토링했습니다.
    크롬 익스텐션으로 돌아가기 때문에, 폴리필은 별도로 필요하지 않다고 판단하였습니다.

--- extension/src/background/setup.js
+++ extension/src/background/setup.js
@@ -1,34 +1,12 @@
-// 소스 가져오기
+const url = '/src/background/'
+const files = ['webfont.js', 'tracker.js', 'jquery.min.js']
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', chrome.extension.getURL('/src/background/webfont.js'), true);
-xhr.onreadystatechange = function()
-{
-    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
-    {
-        window['webfont'] = xhr.responseText;
-        // jquery code
-        var xhr2 = new XMLHttpRequest();
-        xhr2.open('GET', chrome.extension.getURL('/src/website/tracker.js'), true);
-        xhr2.onreadystatechange = function()
-        {
-            if(xhr2.readyState == XMLHttpRequest.DONE && xhr2.status == 200)
-            {
-                window['tracker'] = xhr2.responseText;
-                // jquery code
-                var xhr3 = new XMLHttpRequest();
-                xhr3.open('GET', chrome.extension.getURL('/src/background/jquery.min.js'), true);
-                xhr3.onreadystatechange = function()
-                {
-                    if(xhr3.readyState == XMLHttpRequest.DONE && xhr3.status == 200)
-                    {
-                        window['jquery'] = xhr3.responseText;
-                    }
-                };
-                xhr3.send();
-            }
-        };
-        xhr2.send();
-    }
-};
-xhr.send();
+Promise.all(
+    files.map(filename =>
+        fetch(chrome.extension.getURL(url + filename)).then(res => res.text())
+    )
+).then(([ webfont, tracker, jquery ]) => {
+    window.webfont = webfont
+    window.tracker = tracker
+    window.jquery = jquery
+})
